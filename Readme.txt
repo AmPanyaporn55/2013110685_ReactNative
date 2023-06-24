b1aa32fc517fa0f9593bcc7391f2c624a1cab46c
Ampanyaporn <lu.panyaporn_st@tni.ac.th>
2023-06-24 11:15:09 +0700

    "JavaScript"

--- Readme.txt
+++
@@ -1,4 +0,0 @@
-StudentID : 2013110685
-StudentName : Panyaporn Luangpolsinkul
-DatetoStudy : 17-June-2023
-ClassTopic : Basic javascript
---
+++ javascriptFile/Function.js
@@ -0,0 +1,12 @@
+let avg = (...args) =>{
+    let sum = 0;
+    for(const item of args){
+        sum += item;
+    }
+    return sum / args.length;
+}
+
+console.log(avg(2, 3, 4, 5, 6));
+
+
+
