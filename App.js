094db8d5a65a2e21d6d305c599f98edf93b5ad15
Ampanyaporn <lu.panyaporn_st@tni.ac.th>
2023-08-05 09:52:16 +0700

    Recap Touchable [Week6-August-05-2023]

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import TouchableExample from './components/TouchableExample'
+import TouchablePractice from './components/TouchablePractice'
 
 const App = () => {
   return (
     <View>
-      <TouchableExample/>
+      {/* <TouchableExample/> */}
+      <TouchablePractice/>
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
